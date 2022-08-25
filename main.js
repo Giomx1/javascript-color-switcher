e74ebf7f1f1cf1674b7e63d7fda7854fce13100b
Gio <18yxmoreno@gmail.com>
2022-08-25 10:35:44 -0400

    updated files

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeColor(getColor){
+  let bg = document.getElementById("colorChange")// Grabbing by id which is stored in the body element. Since I want to change the color of the body.
+  let selectColor = getColor.value; //grabbing the color values of the option elements and storing it in new variable
+  bg.style.background = selectColor;//since selectColor stores the color values I will be using it to change the style properties.
+}
+function changeColor(getColor){
+  let bg = document.getElementById("colorChange")
+  let selectColor = getColor.value;
+  bg.style.background = selectColor
+}
